5e21b04428d355a25b4a30604ae70385d9098e54
Vitalii <vitalii.ilchenko@gmail.com>
2019-04-08 11:09:11 +0300

    den review

--- source/js/app.js
+++ source/js/app.js
@@ -29,6 +29,16 @@ $('.reviews__slider').slick({
                 infinite: false,
                 dots: false
             }
+        },
+        {
+            breakpoint: 479,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: false,
+                adaptiveHeight: true
+            }
         }
     ]
 });
@@ -36,13 +46,27 @@ $('.reviews__slider').slick({
 var clickBtn = document.querySelectorAll('.click');
 for(var i = 0; i < clickBtn.length; i++){
     clickBtn[i].onclick = function () {
-        this.previousElementSibling.classList.toggle('active-rew');
+
+        if(this.previousElementSibling.classList.contains('active-rew')){
+            this.previousElementSibling.classList.remove('active-rew');
+            this.innerHTML = '+'
+        }
+        else {
+            for (var i = 0; i < clickBtn.length; i++) {
+                clickBtn[i].previousElementSibling.classList.remove('active-rew');
+                clickBtn[i].innerHTML = '+';
+            }
+            this.previousElementSibling.classList.add('active-rew');
+            this.innerHTML = '-'
+        }
+
     };
 }
 
 
 $('.reviews__slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
     $( ".open" ).removeClass( "active-rew" );
+    $( ".click" ).text('+');
 });
 
 $(document).on('click', 'a[href^="#"]', function (event) {
@@ -50,3 +74,4 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top }, 1000);
 });
+
